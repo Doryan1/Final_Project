efaf05d05f3c5f5f64a25f6884aee25dbf57cc3a
Doryan1 <54554339+Doryan1@users.noreply.github.com>
2020-04-16 18:55:53 -0400

    Add files via upload

---
+++ FINAL PROJECT/JokeGenerator/src/Button2.java
@@ -0,0 +1,13 @@
+import java.awt.event.ActionEvent;
+
+import java.awt.event.ActionListener;
+
+public class Button2 implements ActionListener {
+	
+	public void actionPerformed(ActionEvent e) {
+		dadJokes d = new dadJokes() ;
+				 d.dads() ; 
+
+	}
+
+}
